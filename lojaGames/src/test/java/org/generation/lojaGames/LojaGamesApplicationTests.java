3821688d04eee935ed1184c4977f7651554454f7
Brisola2021 <paulo.brisola@gmail.com>
2022-02-23 12:01:57 -0300

    Atividade Loja Games

---
+++ lojaGames/src/test/java/org/generation/lojaGames/LojaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.lojaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
