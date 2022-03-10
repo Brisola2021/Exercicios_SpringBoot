24b3adb6546864eadf362573f0fb821b60d3203c
Brisola2021 <paulo.brisola@gmail.com>
2022-03-10 12:50:02 -0300

    Deploy inicial - Blog Pessoal

---
+++ deploy_blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,85 @@
+package org.generation.blogPessoal.model;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nome;
+	
+	private String foto;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String usuario;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+    String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Usuario (long id, String nome, String usuario, String senha, String foto) {
+	this.id = id;
+	this.nome = nome;
+	this.usuario = usuario;
+	this.senha = senha;
+	this.foto = foto;
+	}
+	
+	public Usuario() {}
+		
+}
